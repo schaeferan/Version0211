6f708579290593012f9eeeb4213d2b01874b2025
andre <schaeferan94@gmail.com>
2024-04-30 23:09:32 +0200

    anpassung

--- configs/defaults.py
+++ configs/defaults.py
@@ -126,7 +126,7 @@ def get_dataset_config():
   # to store height and width
 
   dataset_config.normalize = True
-  dataset_config.num_interpolation_views = 10#self.num_ref_views
+  dataset_config.num_interpolation_views = 20#self.num_ref_views
 
   # For generalization evaluation
   dataset_config.eval_dataset = "llff"
@@ -229,7 +229,7 @@ def get_train_config():
   train_config = ml_collections.ConfigDict()
 
   train_config.pretrain_dir = ""
-  train_config.switch_scene_iter = 240000
+  train_config.switch_scene_iter = 24000000
   train_config.scheduler = "linear"
   train_config.lr_init = 2.0e-3
   train_config.warmup_epochs = 1
@@ -242,12 +242,12 @@ def get_train_config():
   train_config.lr_delay_mult = 0.1
 
   # The gradient clipping magnitude (disabled if == 0).
-  train_config.step_factor = 3
+  train_config.step_factor = 5
   train_config.grad_max_norm = 0
   train_config.grad_max_val = 0
   train_config.max_steps = 60000 * train_config.step_factor
   train_config.num_epochs = train_config.step_factor
-  train_config.checkpoint_every_steps = 20000
+  train_config.checkpoint_every_steps = 30000
   train_config.log_loss_every_steps = 100
   train_config.render_every_steps = 60000
   train_config.gc_every_steps = 1000
