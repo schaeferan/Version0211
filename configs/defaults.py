085b15b43b88ea87064f1c66a38a451ae9da679d
schaeferan <97290072+schaeferan@users.noreply.github.com>
2024-05-03 21:13:34 +0200

    Update defaults.py
    anpassung

--- configs/defaults.py
+++ configs/defaults.py
@@ -247,7 +247,7 @@ def get_train_config():
   train_config.grad_max_val = 0
   train_config.max_steps = 10000 * train_config.step_factor
   train_config.num_epochs = train_config.step_factor
-  train_config.checkpoint_every_steps = 50000
+  train_config.checkpoint_every_steps = 25000
   train_config.log_loss_every_steps = 100
   train_config.render_every_steps = 50000
   train_config.gc_every_steps = 1000
