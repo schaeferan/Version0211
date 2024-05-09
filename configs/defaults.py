2f70f6e2f395381b8b722a1cfcb1d999a2c95a38
schaeferan <97290072+schaeferan@users.noreply.github.com>
2024-05-09 11:19:55 +0200

    Update defaults.py
    neue paaram

--- configs/defaults.py
+++ configs/defaults.py
@@ -231,7 +231,7 @@ def get_train_config():
   train_config.pretrain_dir = ""
   train_config.switch_scene_iter = 2400000000
   train_config.scheduler = "linear"
-  train_config.lr_init = 3.0e-4
+  train_config.lr_init = 3.0e-5
   train_config.warmup_epochs = 2
   train_config.weight_decay = 0.
   train_config.warmup_steps = 1000
@@ -242,14 +242,14 @@ def get_train_config():
   train_config.lr_delay_mult = 0.1
 
   # The gradient clipping magnitude (disabled if == 0).
-  train_config.step_factor = 7
+  train_config.step_factor = 10
   train_config.grad_max_norm = 0
   train_config.grad_max_val = 0
-  train_config.max_steps = 60000 * train_config.step_factor
+  train_config.max_steps = 20000 * train_config.step_factor
   train_config.num_epochs = train_config.step_factor
   train_config.checkpoint_every_steps = 20000
   train_config.log_loss_every_steps = 100
-  train_config.render_every_steps = 30000
+  train_config.render_every_steps = 20000
   train_config.gc_every_steps = 1000
 
   return train_config
