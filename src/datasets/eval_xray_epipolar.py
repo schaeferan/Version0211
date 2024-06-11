3bae4b0d53a710d05b183816622a72ea249d08a2
andre <schaeferan94@gmail.com>
2024-06-11 12:12:50 +0200

    anpassung movie_file

--- src/datasets/eval_xray_epipolar.py
+++ src/datasets/eval_xray_epipolar.py
@@ -37,7 +37,7 @@ class EvalXRAYEpipolar(FFEpipolar):
 
     projection_matrices = parse_projection_matrices(xml_file_path)
     #movie_path = "/home/andre/Schreibtisch/dataloaderxray/XRAY_movie_verification/eval_trajec2020.npy"
-    camtoworlds_movie = np.load(args.dataset.movie_dir)
+    camtoworlds_movie = np.load(args.dataset.movie_file)
     camtoworlds_movie = camtoworlds_movie[:30]
     #projection_matrices = projection_matrices[::10]
 
