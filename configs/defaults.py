e8292a1b1fb4ab66f6beb891cbf1995eeacb89e7
andre <schaeferan94@gmail.com>
2024-06-22 22:08:14 +0200

    Eval 2 kleiner für ex3.2

--- configs/defaults.py
+++ configs/defaults.py
@@ -126,7 +126,7 @@ def get_dataset_config():
   # to store height and width
 
   dataset_config.normalize = True
-  dataset_config.num_interpolation_views = 10#self.num_ref_views
+  dataset_config.num_interpolation_views = 5#self.num_ref_views
 
   # For generalization evaluation
   dataset_config.eval_dataset = "llff"
