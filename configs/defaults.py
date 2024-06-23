60d90698a41e7ff7f86c56a54d485c233a1cc8fa
andre <schaeferan94@gmail.com>
2024-06-23 11:51:01 +0200

    anpassung für eval3_3 in 3.2

--- configs/defaults.py
+++ configs/defaults.py
@@ -188,7 +188,7 @@ def get_model_config():
 
   #--------------------------------------
   # For epipolar projection
-  model_config.num_projections = 1#127 #num of samples
+  model_config.num_projections = 2#127 #num of samples
   model_config.interpolation_type = "rounding"
   model_config.mask_invalid_projection = False
 
