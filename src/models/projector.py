107d1ef68e16a64b1ccb1fe598b4cde9d13a0bda
andre <schaeferan94@gmail.com>
2024-06-25 21:12:07 +0200

    eigentlich ex4 real

--- src/models/projector.py
+++ src/models/projector.py
@@ -276,11 +276,15 @@ class RayProjector:
     #              )  #(N, B, P, 3, 1)
 
     print("########## pixels : ", pixels.shape)
+    print("########## Inv [:,N,N] : ", inverse_intrisics[:, None, None].shape)
+    print("########## Inv [:,N,N, :] : ", inverse_intrisics[:, None, None, :].shape)
+    print("########## Inv [N,N, :] : ", inverse_intrisics[None, None, :].shape)
+    print("########## Inv [N,:,N, :] : ", inverse_intrisics[None, :, None, :].shape)
     camera_dirs = jnp.matmul(
         #inverse_intrisics[None, None, :],
-        inverse_intrisics[:,None,:, :],
-        #pixels[Ellipsis, None],
-        pixels[..., None],
+        inverse_intrisics[:,None,None],
+        pixels[Ellipsis, None],
+        #pixels[..., None],
         precision=self.precision)  # (N, B, P, 3, 1)
 
     # ref_cameratoworld has shape (N, 3, 4). We only want the rotation part of
